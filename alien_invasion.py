4d178f9f88a7a4f843bc80b9275ca7b77d7ac975
alexcraig481 <acfrady@my.waketech.edu>
2021-09-26 21:04:29 -0400

    Changes to be committed: 	modified:   alien_invasion.py - Added setting instance to init. Refactored to use screen width, height, and background color from settings.py 	new file:   settings.py - Created class with screen_width 1200 , screen_height 800 , and bg_color 230,230,230 as instance properties

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@
 
 import sys
 import pygame
+# Step 3, creating settings.py and importing initial screen settings
+from settings import Settings
 
 
 class AlienInvasion:
@@ -10,14 +12,18 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources"""
         pygame.init()  # Initializes pygame background settings needed
+        self.settings = Settings()
 
         # Actual object assigned to self.screen is called a "surface". It is a visible layer that can contain game
         # elements. Each element in the game, like a ship or an alien, is its own surface.
-        self.screen = pygame.display.set_mode((1200, 800))  # this surface represents entire game window/frame
+        self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_height)
+        # this surface represents entire game window/frame, originally (1200,800) was passed into set_mode for testing
+        # but refactored after settings.py created
+
         pygame.display.set_caption("Alien Invasion")
 
         # Step 2 Set background color
-        self.bg_color = (230, 230, 230)
+        # self.bg_color = (230, 230, 230) Step 3, no longer needed once settings.py created
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -31,7 +37,8 @@ class AlienInvasion:
 
             # Step 2 Redraw the screen during each pass through the loop
             #  - background color
-            self.screen.fill(self.bg_color)  # fill() is a surface method
+            self.screen.fill(self.settings.bg_color)  # fill() is a surface method, originally self.bg_color for
+            # testing, refactored to settings property
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
