998368b7a3662ca378e2d56597f82f8e78b9e7bb
alexcraig481 <acfrady@my.waketech.edu>
2021-09-29 11:24:00 -0400

    Changes to be committed: 	modified:   __pycache__/settings.cpython-39.pyc
    	new file:   bullet.py
    			New class: Bullet, inheriting from Sprite
    			Method: init() - size, location, color
    
    	modified:   settings.py
    			Added bullet settings: speed = 1, width = 3,
    			 height = 15, color = 60,60,60

---
+++ bullet.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):  # inheriting from Sprite - allows grouping of related elements and act on all of them at once
+    """A class to manage bullets fired from the ship"""
+
+        def __init__(self, ai_game):
+            """Create a bullet object at the ship's current position"""
+            super().__init__()
+            self.screen = ai_game.screen
+            self.settings = ai_game.settings
+            self.color = self.settings.bullet_color
+
+            # Rect() draws a bullet according to width & height using 0,0 as top left of bullet
+            self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+            self.rect.midtop = ai_game.ship.rect.midtop  # Make bullet appear to be coming up ship
+
+            # Store the bullet's position as a decimal value
+            self.y = float(self.rect.y)
