09f77758802d357db5c839b43d04f3d117219d5c
alexcraig481 <acfrady@my.waketech.edu>
2021-10-05 18:22:01 -0400

    Changes to be committed: 	modified:   alien_invasion.py 			In create_fleet: 			  Refactored - created helper method 			    _create_alien
    			  Calculated avaliable screen y axis given ship
    			    height, 1 alien height of margin at top, and 2 alien height of margin
    			    between ship top and aliens
    
    			  Calculated number of alien rows from avaliable
    			    y space
    
    			  Added row_number for loop before alien_number
    			    for loop
    
    			In _create_alien:
    			  Pulled alien_height from alien.rect
    			  Added alien.rect.y calculation to determine
    			    alien's row location

--- alien_invasion.py
+++ alien_invasion.py
@@ -113,18 +113,33 @@ class AlienInvasion:
         # Make an alien and find the number of aliens in a row
         # Side margins are 1 alien width, space between each alien is equal to one alien width
         alien = Alien(self)  # ai_game automatically passed in with self
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size   # size contains a tuple with width and height of a rect
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        # Create the first row of aliens
-        for alien_number in range(number_aliens_x):
-            # Create and alien and place it in the row
-            alien = Alien(self)
-            alien.x = alien_width + (2 * alien_width) * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+        # Determine the number of rows of aliens that fit on the screen
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
 
+        # Create the first row of aliens
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+            # Create an alien and place it in the row
+            # alien = Alien(self) refactored out
+            # alien.x = alien_width + (2 * alien_width) * alien_number  refactored out
+            # alien.rect.x = alien.x   refactored out
+            # self.aliens.add(alien)   refactored out
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """Create an alien and place it in the row"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + (2 * alien_width) * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + (2 * alien_height) * row_number
+        self.aliens.add(alien)
 
 if __name__ == '__main__':
     # Make a game instance and run the game
