4995f6e16343ac49699c06ec2a8f925c46f804aa
alexcraig481 <acfrady@my.waketech.edu>
2021-10-05 17:54:47 -0400

    Changes to be committed: 	modified:   alien_invasion.py 			In create_fleet method: 				Calculated avaliable x screen space for 				  aliens given outside margins 1 alien 				  width each and space between aliens is 				  equal to one alien.
    				Calulated number of aliens given
    				  avalaible x screen space
    
    				Used for loop with range to
    				  Create alient instance
    				  Calculate alien.x
    				  Assign alien.rect.x = alien.x
    				  Add alien to self.aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -110,9 +110,20 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Create the fleet of aliens"""
-        # Make an alien
-        alien = Alien(self)  #ai_game automatically passed in with self
-        self.aliens.add(alien)
+        # Make an alien and find the number of aliens in a row
+        # Side margins are 1 alien width, space between each alien is equal to one alien width
+        alien = Alien(self)  # ai_game automatically passed in with self
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Create the first row of aliens
+        for alien_number in range(number_aliens_x):
+            # Create and alien and place it in the row
+            alien = Alien(self)
+            alien.x = alien_width + (2 * alien_width) * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
 
 if __name__ == '__main__':
